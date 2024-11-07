742d8cabae52f04b2a2ae63f52af66855d5df390
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 18:27:30 +0100

    Creo la clase Dama con los atributos y visibilidad requeridos.

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -0,0 +1,19 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+/**
+ *
+ * @author japr
+ */
+
+// Apartado 4.1.
+public class Dama {
+
+    // Atributos con visibilidad privada que almacenarán un color,
+    // una posición y si es o no una Dama Especial.
+    private Color color;
+    private Posicion posicion;
+    private boolean esDamaEspecial;
+
+
+
+}
