03ed8fda6638c6241d1c502858951c8759f8ea59
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 22:06:58 +0100

    Creo el método mostrar menú para la clase Consola.

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -12,4 +12,21 @@ public class Consola {
     // Constructor privado
     private Consola () {}
 
+    // Apartado 5.3.
+    // Método para mostrar el Menú
+    public static void mostrarMenu(){
+        System.out.println("""
+                -----------------------------------
+                ||       MENÚ DE OPCIONES        ||
+                -----------------------------------
+                    1.- Crear Dama por defecto.
+                    2.- Crear Dama eligiendo color.
+                    3.- Mover.
+                    4.- Salir.
+                -----------------------------------   \s
+                ||                               ||
+                -----------------------------------   \s
+                """);
+    }
+
 }
