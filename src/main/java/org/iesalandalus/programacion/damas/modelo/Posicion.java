80c37cff0fbf00e44efa81b8db59c277fe0a2e1b
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 18:10:25 +0100

    Creo los métodos GETTER y SETTER para los atributos.

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -12,4 +12,32 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    // Apartado 3.2.
+    // Métodos GET y SET para los atributos.
+
+    // Método GETTER para la FILA
+    public int getFila(){
+        return fila;
+    }
+
+    // Método SETTER para la FILA
+    public void setFila(int fila){
+        if (fila >= 1 && fila <= 8) this.fila=fila;
+        else
+            throw new IllegalArgumentException("ERROR: Fila no válida.");
+    }
+
+    // Método GETTER para la COLUMNA
+    public char getColumna() {
+        return columna;
+    }
+
+    // Método SETTER para la COLUMNA
+    public void setColumna(char columna) {
+        if (columna >= 'a' && columna <= 'h') this.columna=columna;
+        else
+            throw new IllegalArgumentException("ERROR: Columna no válida.");
+    }
+
+
 }
