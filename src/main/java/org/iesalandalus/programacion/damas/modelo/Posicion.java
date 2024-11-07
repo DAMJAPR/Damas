fb54c99bddff37606fb37bac046087ae6b0088a1
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 18:02:36 +0100

    Creo la clase Posicion con los atributos fila y columna con la visibilidad adecuada

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -0,0 +1,15 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+/**
+ *
+ * @author japr
+ */
+
+// Apartado 3.1.
+public class Posicion {
+
+    // Atributos
+    private int fila;
+    private char columna;
+
+}
