a40302879a33aa8b55a9983f2495656e983c6cd8
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 22:07:49 +0100

    Creo el método elegir opción para la clase Consola.

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.damas;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 /**
  *
  * @author japr
@@ -27,6 +29,24 @@ public class Consola {
                 ||                               ||
                 -----------------------------------   \s
                 """);
+    }
+
+    // Apartado 5.4.
+    // Método para elegir la Opcion del Menú
+    public static int elegirOpcionMenu(){
+        int opcion = 0;
+        do {
+            try{
+                System.out.println("Por favor, introduce el número de la opción que deseas ejecutar (del 1 al 4):");
+                opcion = Entrada.entero();
+                if (opcion < 1 || opcion > 4){
+                    System.out.println("Número de opción incorrecto.");
+                }
+            } catch (Exception e){
+                System.out.println("Error: Introduce un número entero válido.");
+            }
+        } while (opcion < 1 || opcion > 4);
+        return opcion;
     }
 
 }
