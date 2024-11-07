0cf1d9355c18d2cdcded37ea61ef7d78e2a48c5b
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 17:45:35 +0100

    Creo un constructor que almacenará en el atributo anterior la cadena que se le pase por parámetro.

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -16,6 +16,9 @@ public enum Direccion {
     // Apartado 1.2. Atributo privado que almacenará la cadena pasada por parámetro
     private final String cadenaAmostrar;
 
-
+    // Apartado 1.3. Constructor con visibilidad privada que asigna la cadena al atributo
+    Direccion (String cadenaAmostrar){
+        this.cadenaAmostrar = cadenaAmostrar;
+    }
 
 }
