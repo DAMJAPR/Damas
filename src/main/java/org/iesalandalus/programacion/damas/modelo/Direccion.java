ad3ed22b08b0f0a83b56ac26dece7d89cfb847f1
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 17:41:36 +0100

    Modificado el archivo README.md para añadir mi nombre.
    Creo un enumerado llamado Dirección que contiene los literales NORESTE, SURESTE, SUROESTE y NOROESTE parametrizados con la representación en cadena de los mismos.

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -0,0 +1,16 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+/**
+ *
+ * @author japr
+ */
+
+// Apartado 1.1
+public enum Direccion {
+
+    NORESTE("Noreste"),
+    SURESTE("Sureste"),
+    SUROESTE("Suroeste"),
+    NOROESTE("Noroeste");
+
+}
