2e45d12176baf127238dbd2ea8777b0807cda79a
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 22:14:12 +0100

    Creo la clase MainApp y el atributo de clase dama.

---
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -0,0 +1,14 @@
+package org.iesalandalus.programacion.damas;
+
+/**
+ *
+ * @author japr
+ */
+
+// Apartado 6.1.
+public class MainApp {
+
+    // Atributo dama
+    private static Dama dama;
+
+}
