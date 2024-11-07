8dc1d79edf6ef89dcfd41ab9cbf14f5279f7ac4f
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 18:37:21 +0100

    Creo los métodos get y set para cada atributo con la visibilidad adecuada

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -14,6 +14,42 @@ public class Dama {
     private Posicion posicion;
     private boolean esDamaEspecial;
 
+    // Apartado 4.2.
+    // Método GET y SET para los atributos.
+
+    // Método GETTER para el color
+    public Color getColor(){
+        return color;
+    }
+    // Método SETTER para el color
+    public void setColor(Color color){
+        if (color == null) {
+            throw new IllegalArgumentException("ERROR: El color no puede ser nulo.");
+        }
+        this.color = color;
+    }
+
+    // Método GETTER para la posición
+    public Posicion getPosicion(){
+        return posicion;
+    }
+    // Método SETTER para la posición
+    public void setPosicion(Posicion posicion){
+        if (posicion == null){
+            throw new IllegalArgumentException("ERROR: La posición no puede ser nula.");
+        }
+        this.posicion = posicion;
+    }
+
+    // Método GETTER para la Dama Especial
+    public boolean isEsDamaEspecial(){
+        return esDamaEspecial;
+    }
+    // Método SETTER para la Dama Especial
+    public void setEsDamaEspecial(boolean esDamaEspecial) {
+        this.esDamaEspecial = esDamaEspecial;
+    }
+
 
 
 }
