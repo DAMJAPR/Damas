bbdaa6d16018120feb9fb15a87f6edbbd537af4a
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 17:58:30 +0100

    Creo el método toString para el enumerado Color.

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Color.java
@@ -11,15 +11,16 @@ public enum Color {
     BLANCO("Blanco"),
     NEGRO("Negro");
 
-    // Apartado 2.2.
+    // Apartado 2.2.  Atributo privado para almacenar la representación en cadena del literal
     private final String cadenaAMostrar;
 
-    // Apartado 2.3.
+    // Apartado 2.3. Constructor privado que inicializa el atributo con la cadena pasada por parámetro
     Color (String cadenaAMostrar){
         this.cadenaAMostrar = cadenaAMostrar;
     }
 
-
-
-
+    // Apartado 2.4. Método toString que devuelve la representación en forma de cadena del literal
+    public String toString(){
+        return cadenaAMostrar;
+    }
 }
