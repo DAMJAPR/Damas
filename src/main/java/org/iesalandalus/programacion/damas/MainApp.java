0555db8c64ffd64a24837ceb981ff41d2d6c19e0
DAMJAPR <151417386+DAMJAPR@users.noreply.github.com>
2024-11-07 22:17:14 +0100

    Creo el método ejecutar opción en la clase MainApp.

--- src/main/java/org/iesalandalus/programacion/damas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -1,14 +1,38 @@
 package org.iesalandalus.programacion.damas;
 
+import org.iesalandalus.programacion.damas.modelo.Dama;
+
 /**
  *
  * @author japr
  */
 
-// Apartado 6.1.
+// Apartado 6.
 public class MainApp {
 
-    // Atributo dama
+    // Apartado 6.1.
+    // Atributo de clase dama
     private static Dama dama;
+
+    // Apartado 6.2.
+    // Método Ejecutar Opción
+    private static void ejecutarOpcion(int opcion) {
+        switch (opcion){
+            case 1:
+                crearDamaDefecto();
+                break;
+            case 2:
+                crearDamaColor();
+                break;
+            case 3:
+                mover();
+                break;
+            case 4:
+                System.out.println(Consola.despedirse());
+                break;
+        }
+    }
+
+
 
 }
